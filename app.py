f8437737ac5865738b3f6aa4ba67b7978bf03b8d
SankaranarayananA <76698114+SankaranarayananA@users.noreply.github.com>
2021-05-06 23:32:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,6 @@
 import streamlit as st
 import sklearn
 import joblib
-import spacy
-import string
-from spacy.lang.en.stop_words import STOP_WORDS
-stopwords= list(STOP_WORDS)
-punct = string.punctuation
-nlp = spacy.load("en_core_web_lg")
-def text_cleaning(vario): # accept only 1 review at a run
-  doc = nlp(vario) # calling spacy model to work on a SENTENCE 
- 
-  
-  tokens = [] # list of tokens
-
-  # lowering case all tokens 
-  
-  for token in doc:
-
-# if root form(token) of that word is not pronoun then it is going to convert that into lowercase
-    if token.lemma_ !="-PRON-":
-      temp = token.lemma_.lower().strip()
-    else:
-# If that word is proper noun,then it directly taking lower case, because there is no lemma for proper noun
-      temp = token.lower_
-    tokens.append(temp)
-
-
-
-  cleaned_tokens= [] 
-  # removing all punctuation and stopword tokens  
-  for token in tokens:
-    if token not in stopwords and token not in punct:
-      cleaned_tokens.append(token)
-  return cleaned_tokens  
 model = joblib.load('Sentia')
 st.title('Sentiment Analysis')
 ip = st.text_input("Enter the Review")
